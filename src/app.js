ce6e81891742a2e7fc42781e695ed31d058934bb
minterger <minterger@gmail.com>
2021-06-17 16:54:39 -0300

    add headers

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ app.set(express.json());
 app.set(express.urlencoded({extended: false}));
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(require('./routes/main.routes'));
 app.use(require('./routes/peliculas.routes'));
 app.use(require('./routes/series.routes'));
