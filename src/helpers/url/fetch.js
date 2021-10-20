3959d0be65ae0096e9202c8850db4fec588dd271
minterger <minterger@gmail.com>
2021-10-20 01:20:39 -0300

    Update fetch.js

--- src/helpers/url/fetch.js
+++ src/helpers/url/fetch.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 async function fetchurl(param) {
   let uri = param == null ? "" : param;
   try {
-    const res = await axios.get(`https://pelisplushd.net/${uri}`);
+    const res = await axios.get(`https://pelisplushd.net${uri}`);
     return res;
   } catch (error) {
+    log
     if (error.response) return error.response;
     return {status: 500, statusText: 'error interno'}
   }
