2520c6cb216dc6517e19ab154fbe2793c29fd44e
minterger <minterger@gmail.com>
2021-12-06 01:48:23 -0300

    Update redis.js

--- src/redis.js
+++ src/redis.js
@@ -1,18 +1,24 @@
 const redis = require("redis");
-const { promisify } = require("util");
 
 const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
 });
 
-client.on('error', (err) => {
-  console.log(err);
-})
+(async () => {
+  client.on("error", (err) => {
+    console.log(err);
+  });
 
-const redisGet = promisify(client.get).bind(client);
+  await client.connect();
+})();
 
-const redisSet = promisify(client.set).bind(client);
+const redisGet = async (key) => {
+  return await client.get(key);
+};
+
+const redisSet = async (key, value) => {
+  return await client.set(key, value);
+};
 
 module.exports = { redisGet, redisSet };
