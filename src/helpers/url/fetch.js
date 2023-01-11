58a75a7d5b5876dfd3053e1c1e627cff9a79e9a8
Leonel Menci <minterger@gmail.com>
2023-01-10 23:02:30 -0300

    update url

--- src/helpers/url/fetch.js
+++ src/helpers/url/fetch.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function fetchurl(param) {
   let uri = param == null ? "" : param;
   try {
-    const res = await axios.get(`https://pelisplushd.net${uri}`);
+    const res = await axios.get(`https://www.pelisplus.lat${uri}`);
     return res;
   } catch (error) {
     if (error.response) return error.response;
-    return {status: 500, statusText: 'error interno'}
+    return { status: 500, statusText: "error interno" };
   }
 }
 
